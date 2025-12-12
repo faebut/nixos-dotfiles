81de45a21af43c99fcbdc87cca222039b402768c
Fabian Trost <faebutrost@gmail.com>
2025-12-12 12:19:03 +0100

    change thunderbird

--- home-modules/desktop/thunderbird.nix
+++ home-modules/desktop/thunderbird.nix
@@ -6,5 +6,16 @@
 {
   programs.thunderbird = {
     enable = true;
+
+    profiles = {
+      faebut = {
+        isDefault = true;
+
+        # extraConfig = ''
+        #   // Disable automatic updates for this profile
+        #   user_pref("app.update.enabled", false);
+        # '';
+      };
+    };
   };
 }

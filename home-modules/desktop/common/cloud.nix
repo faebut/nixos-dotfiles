b054d94467f1253cc4ec597e2920d73cca196f33
Fabian Trost <faebutrost@gmail.com>
2025-12-25 12:14:53 +0100

    add filen service and get secrets from nix-secrets

--- home-modules/desktop/common/cloud.nix
+++ home-modules/desktop/common/cloud.nix
@@ -1,9 +1,41 @@
 {
+  inputs,
   config,
   pkgs,
+  lib,
   unstablePkgs,
   ...
-}: {
+}:
+let
+  secretspath = builtins.toString inputs.nix-secrets;
+in
+let
+  filenMountScript = pkgs.writeShellScript "filen-mount" ''
+    ${pkgs.filen-cli}/bin/filen mount "$HOME/Cloud/Filen" \
+      --email "$(cat ${config.sops.secrets.filen-email.path})" \
+      --password "$(cat ${config.sops.secrets.filen-password.path})"
+  '';
+in
+{
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+  ];
+
+  sops = {
+    defaultSopsFile = "${secretspath}/secrets.yaml";
+    defaultSopsFormat = "yaml";
+    validateSopsFiles = false;
+
+    age = {
+      keyFile = "/home/faebut/.config/sops/age/keys.txt";
+    };
+
+    secrets = {
+      filen-email = { };
+      filen-password = { };
+    };
+  };
+
   home.packages = with pkgs; [
     filen-cli # filen command line client
   ];
@@ -21,8 +53,8 @@
         "${pkgs.coreutils}/bin/mkdir -p %h/Cloud/Filen"
         "${pkgs.coreutils}/bin/rm -f %h/Cloud/Filen/.keep"
       ];
-      ExecStart = "${pkgs.filen-cli}/bin/filen mount %h/Cloud/Filen";
-      ExecStopPost = "${pkgs.fuse}/bin/fusermount -uz %h/Cloud/Filen";
+      ExecStart = "${filenMountScript}";
+      ExecStopPost = "/run/wrappers/bin/fusermount -uz %h/Cloud/Filen";
       Restart = "on-failure";
       RestartSec = "10s";
     };

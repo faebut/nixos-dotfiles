a7066a762c4469494be093ed7b6858f86c88053f
Fabian Trost <faebutrost@gmail.com>
2025-12-26 13:40:33 +0100

    define mail-accounts in nix-secrets

--- home-modules/desktop/common/thunderbird.nix
+++ home-modules/desktop/common/thunderbird.nix
@@ -1,19 +1,25 @@
 {
   pkgs,
   vars,
+  config,
+  lib,
+  inputs,
   ...
-}: {
+}: let
+  secretspath = builtins.toString inputs.nix-secrets;
+in {
+  imports = [
+    inputs.sops-nix.homeManagerModules.sops
+    # Import account configuration from private repo
+    "${secretspath}/thunderbird-accounts.nix"
+  ];
+
   programs.thunderbird = {
     enable = true;
 
     profiles = {
       faebut = {
         isDefault = true;
-
-        # extraConfig = ''
-        #   // Disable automatic updates for this profile
-        #   user_pref("app.update.enabled", false);
-        # '';
       };
     };
   };

89bd9af3ce85c7060d6e0bfc959b3af2dd315c0e
Fabian Trost <ftrost@proton.me>
2026-04-17 15:30:29 +0200

    fix lychee problem

--- home-modules/desktop/3dprinting/3dprinting.nix
+++ home-modules/desktop/3dprinting/3dprinting.nix
@@ -1,19 +1,6 @@
-{pkgs, unstablePkgs, ...}: let
-  lycheeslicer-fixed = pkgs.symlinkJoin {
-    name = "lycheeslicer-fixed";
-    paths = [
-      (pkgs.runCommand "lycheeslicer-desktop-fix" {} ''
-        mkdir -p "$out/share/applications"
-        find ${unstablePkgs.lycheeslicer}/share/applications -name "*.desktop" | while read -r f; do
-          sed 's/Exec=lychee/Exec=LycheeSlicer/g' "$f" > "$out/share/applications/$(basename "$f")"
-        done
-      '')
-      unstablePkgs.lycheeslicer
-    ];
-  };
-in {
+{pkgs, unstablePkgs, ...}: {
   home.packages = [
     pkgs.orca-slicer
-    lycheeslicer-fixed
+    unstablePkgs.lycheeslicer
   ];
 }

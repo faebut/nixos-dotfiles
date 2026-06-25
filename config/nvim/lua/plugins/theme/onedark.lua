db4d420cf6b9af47eefc6041550d03bc2eb9992a
Fabian Trost <ftrost@proton.me>
2026-06-25 11:31:55 +0200

    switch to darker variant of theme

--- config/nvim/lua/plugins/theme/onedark.lua
+++ config/nvim/lua/plugins/theme/onedark.lua
@@ -5,7 +5,7 @@ return {
   priority = 1000, -- make sure to load this before all the other start plugins
   config = function()
     require('onedark').setup {
-      style = 'warmer'
+      style = 'darker'
     }
     require('onedark').load()
   end

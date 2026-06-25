c7366aa50f26de44feada843a17c4757286ef154
Fabian Trost <ftrost@proton.me>
2026-06-25 11:42:30 +0200

    now onedark works

--- config/nvim/lua/plugins/theme/onedark.lua
+++ config/nvim/lua/plugins/theme/onedark.lua
@@ -1,12 +1,12 @@
 -- Using Lazy
 return {
-  "navarasu/onedark.nvim",
-  name = "onedark",
-  priority = 1000, -- make sure to load this before all the other start plugins
-  config = function()
-    require('onedark').setup {
-      style = 'darker'
-    }
-    require('onedark').load()
-  end
+	"navarasu/onedark.nvim",
+	name = "onedark",
+	priority = 1000, -- make sure to load this before all the other start plugins
+	config = function()
+		require("onedark").setup({
+			style = "darker",
+		})
+		require("onedark").load()
+	end,
 }
